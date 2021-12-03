db979f5b5d40c6477381e6b0e76d4177d2a494c7
antoine <pirlotantoine2@gmail.com>
2021-12-03 18:26:08 +0100

    Refactor

--- sql_script/etudiants.sql
+++ sql_script/etudiants.sql
@@ -1,3 +1,3 @@
-INSERT INTO project_sql.etudiants (id_etudiant, nom, prenom, email, mot_de_passe, bloc, nombre_de_credits_valides) VALUES (1, 'Pirlot', 'Antoine', 'antoine@vinc.be', '$2a$10$X3O5cj8RU.tLPgCgKAQwP.bmo0LSHoIBhp4PeSiTjj6DSkzik2u0u', null, 60);
-INSERT INTO project_sql.etudiants (id_etudiant, nom, prenom, email, mot_de_passe, bloc, nombre_de_credits_valides) VALUES (2, 'Vogel', 'Anouck', 'anouck@ecole.be', '$2a$10$hkeGjz2tqJb4bhq0Izw9PepinBm0gMuqCxQHUHCO8iKndaF581OVS', null, 38);
-INSERT INTO project_sql.etudiants (id_etudiant, nom, prenom, email, mot_de_passe, bloc, nombre_de_credits_valides) VALUES (3, 'Kamerling', 'Hugo', 'hugo@ecole.be', '$2a$10$IaRF3Hkmgoox8C1Fknx6NeoI2YTy1XWIPhK6Ujjmo29Xk6CQ/gClO', null, 9);
+INSERT INTO project_sql.etudiants (nom, prenom, email, mot_de_passe) VALUES ('Pirlot', 'Antoine', 'antoine@vinci.be', '$2a$10$X3O5cj8RU.tLPgCgKAQwP.bmo0LSHoIBhp4PeSiTjj6DSkzik2u0u');
+INSERT INTO project_sql.etudiants (nom, prenom, email, mot_de_passe) VALUES ('Vogel', 'Anouck', 'anouck@ecole.be', '$2a$10$hkeGjz2tqJb4bhq0Izw9PepinBm0gMuqCxQHUHCO8iKndaF581OVS');
+INSERT INTO project_sql.etudiants (nom, prenom, email, mot_de_passe) VALUES ('Kamerling', 'Hugo', 'hugo@ecole.be', '$2a$10$IaRF3Hkmgoox8C1Fknx6NeoI2YTy1XWIPhK6Ujjmo29Xk6CQ/gClO');
