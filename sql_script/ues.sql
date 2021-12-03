dec3803c472deb9f0f3b30d1405eb655184bfeda
antoine <pirlotantoine2@gmail.com>
2021-12-03 20:32:51 +0100

    Update

--- sql_script/ues.sql
+++ sql_script/ues.sql
@@ -3,10 +3,10 @@ INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BIN
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV1020-1', 'Analyse et programmation orientée objet', 1, 6);
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV1030-1', 'Gestion de données: bases', 1, 5);
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV1040-1', 'Structures de données: bases', 1, 6);
-INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV1050-1', 'Développement Web: bases', 1, 5);
+INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV1051-1', 'Développement Web: bases', 1, 5);
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV1060-1', 'OS', 1, 5);
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV1071-1', 'Description Des Ordinateurs', 1, 2);
-INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV1072', 'Langage Machine', 1, 3);
+INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV1072-1', 'Langage Machine', 1, 3);
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV1080-2', 'Eco', 1, 5);
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV1090-1', 'Mathématiques', 1, 4);
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV1100-1', 'Mathématiques 2', 1, 6);
@@ -33,7 +33,7 @@ INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BIN
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV3200-3', 'Organisation des entreprises', 3, 5);
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV3050-1', 'Anglais 3', 3, 2);
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV3140-1', 'Programmation: questions spéciales', 3, 5);
-INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV3150-1-1', 'Développement Web: questions spéciales', 3, 3);
+INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV3150-1', 'Développement Web: questions spéciales', 3, 3);
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV3190-1', 'Projet de fin d''études', 3, 5);
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV3080-1', 'Intégration en milieu professionnel', 3, 30);
 INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV3090-1', 'Développement à l''aide d''un moteur de jeux, Unity', 3, 4);
