2f9fb88e6eb7a9b1b1bfb32d85b804d0c0cc7951
Antoine <pirlotantoine2@gmail.com>
2021-12-04 18:34:28 +0100

    Add demo sql script for data

---
+++ sql_script/demo/ues.sql
@@ -0,0 +1,8 @@
+INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV11', 'BD1', 1, 31);
+INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV12', 'APOO', 1, 16);
+INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV13', 'Algo', 1, 13);
+
+INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV21', 'BD2', 2, 42);
+
+INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV311', 'Anglais', 3, 16);
+INSERT INTO project_sql.ues (code_ue, nom, bloc, nombre_de_credits) VALUES ('BINV32', 'Stage', 3, 44);
