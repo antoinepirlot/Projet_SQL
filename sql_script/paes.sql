8b8fe16e4b57fbefa88df81f0c60bc85f82acff4
Antoine <pirlotantoine2@gmail.com>
2021-12-04 13:21:54 +0100

    DELETE useless pae script

--- sql_script/paes.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO project_sql.paes (code_pae, id_etudiant, valide, nombre_de_credits_total) VALUES (2, 2, false, 0);
-INSERT INTO project_sql.paes (code_pae, id_etudiant, valide, nombre_de_credits_total) VALUES (3, 3, false, 0);
-INSERT INTO project_sql.paes (code_pae, id_etudiant, valide, nombre_de_credits_total) VALUES (4, 4, false, 0);
-INSERT INTO project_sql.paes (code_pae, id_etudiant, valide, nombre_de_credits_total) VALUES (5, 5, false, 0);
-INSERT INTO project_sql.paes (code_pae, id_etudiant, valide, nombre_de_credits_total) VALUES (6, 6, false, 0);
-INSERT INTO project_sql.paes (code_pae, id_etudiant, valide, nombre_de_credits_total) VALUES (1, 1, false, 0);
