e280d48a088d731d04ae1e0b7abada4f8eb6af6d
Antoine <pirlotantoine2@gmail.com>
2021-12-05 17:14:24 +0100

    Refactor

--- sql_script/demo.sql
+++ sql_script/demo.sql
@@ -29,3 +29,13 @@ INSERT INTO project_sql.ues_validees (id_etudiant, id_ue) VALUES (4, 2);
 INSERT INTO project_sql.ues_validees (id_etudiant, id_ue) VALUES (4, 3);
 INSERT INTO project_sql.ues_validees (id_etudiant, id_ue) VALUES (4, 4);
 INSERT INTO project_sql.ues_validees (id_etudiant, id_ue) VALUES (4, 6);
+
+
+
+INSERT INTO project_sql.etudiants (id_etudiant, nom, prenom, email, mot_de_passe, bloc, nombre_de_credits_valides) VALUES (DEFAULT, 'Pirlot', 'Antoine', 'antoine@vinci.be', '$2a$10$X3O5cj8RU.tLPgCgKAQwP.bmo0LSHoIBhp4PeSiTjj6DSkzik2u0u', DEFAULT, DEFAULT);
+INSERT INTO project_sql.etudiants (id_etudiant, nom, prenom, email, mot_de_passe, bloc, nombre_de_credits_valides) VALUES (DEFAULT, 'Vogel', 'Anouck', 'anouck@ecole.be', '$2a$10$hkeGjz2tqJb4bhq0Izw9PepinBm0gMuqCxQHUHCO8iKndaF581OVS', DEFAULT, DEFAULT);
+INSERT INTO project_sql.etudiants (id_etudiant, nom, prenom, email, mot_de_passe, bloc, nombre_de_credits_valides) VALUES (DEFAULT, 'Kamerling', 'Hugo', 'hugo@ecole.be', '$2a$10$IaRF3Hkmgoox8C1Fknx6NeoI2YTy1XWIPhK6Ujjmo29Xk6CQ/gClO', DEFAULT, DEFAULT);
+INSERT INTO project_sql.etudiants (id_etudiant, nom, prenom, email, mot_de_passe, bloc, nombre_de_credits_valides) VALUES (DEFAULT, 'Vermeich', 'Lisa', 'lisa@ecole.be', '$2a$10$qnPOFtqyKk6tmNn8izLmz.Uqo1YthOd87o.7to7tcgPbsv2A3kvlK', DEFAULT, DEFAULT);
+INSERT INTO project_sql.etudiants (id_etudiant, nom, prenom, email, mot_de_passe, bloc, nombre_de_credits_valides) VALUES (DEFAULT, 'Jordan', 'Alexander', 'alexander@ecole.be', '$2a$10$./DB3hMrmTdaBFEMSi9ICucHNzgwMd1E4CmEWApnuezg24mBFv70W', DEFAULT, DEFAULT);
+INSERT INTO project_sql.etudiants (id_etudiant, nom, prenom, email, mot_de_passe, bloc, nombre_de_credits_valides) VALUES (DEFAULT, 'Lorie', 'Maude', 'maude@ecole.be', '$2a$10$9g/Xh7zU2daUIUvbJInS4.1AYq512oMaez2Ox0kRNqh7e04cBzJh.', DEFAULT, DEFAULT);
+
