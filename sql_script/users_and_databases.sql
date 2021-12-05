3f697f45908a0118022493328c341002e33f520c
Antoine <pirlotantoine2@gmail.com>
2021-12-05 16:17:09 +0100

    Refactor

--- sql_script/users_and_databases.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER antoinepirlot WITH PASSWORD 'at5BER69h';
-CREATE DATABASE dbantoinepirlot;
-GRANT ALL PRIVILEGES ON DATABASE dbantoinepirlot TO antoinepirlot;
-
-CREATE USER nicolasdimitriadis WITH PASSWORD '0JE9SRO2G';
-CREATE DATABASE dbnicolasdimitriadis;
-GRANT ALL PRIVILEGES ON DATABASE dbnicolasdimitriadis TO nicolasdimitriadis;
